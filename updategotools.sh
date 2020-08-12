24f8ec836af266dd3cfa6e1b5214b024ca983c4e
Hsin <hsin@erina.lan>
2020-08-11 22:20:13 -0700

    support modules

--- updategotools.sh
+++ updategotools.sh
@@ -1,35 +1,49 @@
 #!/bin/sh
 
+export GO111MODULE=on
+
 echo "Updating GoCode"
 go get -u github.com/nsf/gocode
 
 #go get -u github.com/alecthomas/gometalinter
 
 echo "Updating GoImports"
-go get -u golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/goimports@master
 
 echo "Updating Guru"
-go get -u golang.org/x/tools/cmd/guru
+go get golang.org/x/tools/cmd/guru@master
 
 echo "Updating GoRename"
-go get -u golang.org/x/tools/cmd/gorename
+go get golang.org/x/tools/cmd/gorename@master
 
 echo "Updating GoLint"
-go get -u github.com/golang/lint/golint
+go get golang.org/x/lint/golint@master
 
 echo "Updating GoDef"
-go get -u github.com/rogpeppe/godef
+go get github.com/rogpeppe/godef@master
 
 echo "Updating ErrCheck"
-go get -u github.com/kisielk/errcheck
+go get github.com/kisielk/errcheck@master
 
 echo "Updating GoTags"
-go get -u github.com/jstemmer/gotags
+go get github.com/jstemmer/gotags@master
 #go get -u github.com/klauspost/asmfmt/cmd/asmfmt
 
 echo "Updating Motion"
-go get -u github.com/fatih/motion
+go get github.com/fatih/motion@master
 
 echo "Update gogetdoc"
-go get -u github.com/zmb3/gogetdoc
+go get github.com/zmb3/gogetdoc@master
 #go get -u github.com/josharian/impl
+
+echo "Update gopls"
+go get golang.org/x/tools/gopls@latest
+
+echo "Update gomodifytags"
+go get github.com/fatih/gomodifytags@master
+
+echo "Update present"
+go get golang.org/x/tools/cmd/present 
+
+#go get github.com/ChimeraCoder/gojson/gojson
+
